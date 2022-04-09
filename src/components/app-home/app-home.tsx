777b41ec1221f3002ec23df87c58de9fb6ef772f
Jack Driscoll <jdriscoll98@ufl.edu>
2022-04-09 13:41:06 -0400

    stuf

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,5 +1,5 @@
-import { Component, h, State } from '@stencil/core';
-import { createProposal } from '../../services/proposal.services'
+import { Component, h, Host, State } from '@stencil/core';
+import ProposalService from '../../services/proposal.services';
 
 @Component({
   tag: 'app-home',
@@ -8,26 +8,72 @@ import { createProposal } from '../../services/proposal.services'
 })
 export class AppHome {
   @State() name: string;
+  @State() signer;
+  proposalService = new ProposalService();
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log(this.name);
-    createProposal();
+    this.proposalService.createProposal(this.signer);
   }
 
   handleChange(event) {
     this.name = event.target.value;
   }
 
+  async connectToWallet() {
+    this.signer = await this.proposalService.getSigner();
+  }
+
+  async disconnect() {
+    this.signer = null;
+  }
+
+  async componentDidLoad() {
+    this.proposalService.onProposalsCreated(
+      e => {
+        console.log('onCreated');
+        console.log(e);
+      },
+      e => {
+        console.log('onData');
+        console.log(e);
+      },
+      e => {
+        console.log('onError');
+        console.log(e);
+      },
+    );
+
+    this.proposalService.onPastProposals(e => {
+      console.log('OnPastProposals');
+      console.log(e);
+    });
+  }
+
   render() {
     return (
-      <form onSubmit={e => this.handleSubmit(e)}>
-        <label>
-          Proposal Name:
-          <input type="text" value={this.name} onInput={event => this.handleChange(event)} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <Host>
+        <button
+          onClick={() => {
+            if (this.signer) {
+              this.disconnect();
+            } else {
+              this.connectToWallet();
+            }
+          }}
+        >
+          {this.signer ? 'Disconnect' : 'Connect'}
+        </button>
+        {this.signer && (
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <label>
+              Proposal Name:
+              <input type="text" value={this.name} onInput={event => this.handleChange(event)} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        )}
+      </Host>
     );
   }
 }
